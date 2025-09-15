7ec723b2f80f2da0ef570aff09db92723b901cdd
Viraj Perera <viraj.perera.dev@gmail.com>
2025-09-15 10:13:12 +0200

    updated version

--- src/Game/RightColl/Settings/Settings.tsx
+++ src/Game/RightColl/Settings/Settings.tsx
@@ -9,29 +9,54 @@ import { FaCoffee } from 'react-icons/fa';
 const Settings: FC = () => {
   return (
     <Fragment>
-      <AiLevel />
-      <PlayButton vsAi />
-      <div className="my-4 px-4 text-black">
-        <p className="italic text-xl font-roboto-slab">
-        "Si dice che gli scacchi, come tutti i grandi giochi, abbiano avuto una nascita leggendaria. Un giorno, il geniale rivoluzionario degli scacchi, Viraj Perera, mentre sorseggiava un caffè e rifletteva sulla vita, si chiese: 'Perché non iniziare una partita con il pezzo nero?' La risposta, ovviamente, arrivò come una folgorazione: il nero rappresenta il mistero, l'intrigo e una strategia astuta che mette subito alla prova il giocatore. Era chiaro, il primo movimento doveva essere audace!"
+
+      {/* Subsection: Difficulty Level */}
+      <section className="my-6">
+        <h3 className="text-2xl font-semibold mb-4">Scegli il Livello di Difficoltà</h3>
+        <AiLevel />
+      </section>
+
+      {/* Subsection: Play Button */}
+      <section className="my-6">
+        <h2 className="text-2xl font-semibold mb-4">Inizia la Partita</h2>
+        <PlayButton vsAi />
+      </section>
+
+      {/* Subsection: Quote */}
+      <section className="my-6 text-black">
+        {/* Main Heading */}
+        <h1 className="text-2xl font-bold text-start">
+          Gioca a Scacchi con il Nero!
+        </h1>
+        <h2 className="text-xl font-semibold mb-2">
+          La Rivoluzione degli Scacchi
+        </h2>
+        <p className="italic font-roboto-slab">
+          "Si dice che gli scacchi, come tutti i grandi giochi, abbiano avuto una nascita leggendaria. 
+          Un giorno, il geniale rivoluzionario degli scacchi, Viraj Perera, mentre sorseggiava un caffè 
+          e rifletteva sulla vita, si chiese: 'Perché non iniziare una partita con il pezzo nero?' 
+          La risposta, ovviamente, arrivò come una folgorazione: il nero rappresenta il mistero, 
+          l'intrigo e una strategia astuta che mette subito alla prova il giocatore. Era chiaro, il 
+          primo movimento doveva essere audace!"
         </p>
-      </div>
-      {/* Support the Project Section */}
-      <div className="my-6 p-4 text-center bg-yellow-50 border border-yellow-200 rounded-lg shadow-md">
-        <p className="text-lg font-semibold text-gray-800 mb-2">
-          Se ti piace questo progetto e vuoi supportarlo, considera di offrirmi un caffè! ☕️
+      </section>
+
+      {/* Support Section */}
+      <section className="my-6 p-4 text-center bg-yellow-50 rounded-lg shadow-md">
+        <h4 className="text-2xl font-medium mb-4 font-roboto-slab text-yellow-950">Supporta il Progetto!</h4>
+        <p className="text-lg text-gray-800 mb-4">
+          Se apprezzi questo progetto e vuoi darci una mano, offrici un caffè per le lunghe notti insonni passate per realizzare tutto questo! ☕️
         </p>
         <a
           href="https://buymeacoffee.com/virajperera"
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-flex items-center px-4 py-2 mt-2 font-bold text-white bg-yellow-500 rounded-lg hover:bg-yellow-600 transition"
+          className="inline-flex items-center px-4 py-2 font-bold text-white bg-yellow-500 rounded-lg hover:bg-yellow-600 transition border border-yellow-950 border-2"
         >
-          <span className="mr-2">Buy Me a Coffee</span>
-          {/* Use React Icon here */}
-          <FaCoffee className="w-6 h-6" />
+          <span className="mr-2 text-yellow-950">Buy Me a Coffee</span>
+          <FaCoffee className="w-6 h-6 text-yellow-950" />
         </a>
-      </div>
+      </section>
     </Fragment>
   );
 };
